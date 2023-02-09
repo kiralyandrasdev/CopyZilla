5f174ebd3f4f522b5910fc44c92992bc0df530d8
Daniel Reha <rehadaniel.personal@gmail.com>
2023-02-09 22:33:16 +0100

    Moved API base url and Stripe Customer Portal URL to .env file

--- web/src/features/api/apiSlice.js
+++ web/src/features/api/apiSlice.js
@@ -1,9 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { apiBaseUrl } from '../../config/envConfig';
 
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://localhost:7107/api", headers: {
+        baseUrl: apiBaseUrl, headers: {
             'Content-Type': 'application/json',
         }
     }),
