a3aa650f104e749d8b19e818f7e920512aa85b8e
kiralyandrasdev <kiralyandrasdev@gmail.com>
2023-02-13 23:15:19 +0100

    init changes

--- backend/CopyZillaBackend/src/CopyZillaBackend.Infrastructure/Firebase/FirebaseService.cs
+++ backend/CopyZillaBackend/src/CopyZillaBackend.Infrastructure/Firebase/FirebaseService.cs
@@ -1,13 +1,19 @@
 ﻿using CopyZillaBackend.Application.Contracts.Firebase;
+using FirebaseAdmin;
 using FirebaseAdmin.Auth;
 
 namespace CopyZillaBackend.Infrastructure.Firebase
 {
     public class FirebaseService : IFirebaseService
     {
+        public async Task<UserRecord> GetFirebaseUserAsync(string id)
+        { 
+            return await FirebaseAuth.GetAuth(FirebaseApp.GetInstance("default")).GetUserAsync(id);
+        }
+
         public async Task DeleteFirebaseUserAsync(string id)
         {
-            await FirebaseAuth.DefaultInstance.DeleteUserAsync(id);
+            await FirebaseAuth.GetAuth(FirebaseApp.GetInstance("default")).DeleteUserAsync(id);
         }
     }
 }
