d3c137f6cf938ca92f614917bc98ab33a3802f98
kiralyandrasdev <kiralyandrasdev@gmail.com>
2023-02-15 20:49:46 +0100

    validator should only validate the request -> moved stripe and firebase check to handler

--- backend/CopyZillaBackend/src/CopyZillaBackend.Application/Features/User/Commands/DeleteUserCommand/DeleteUserCommandHandler.cs
+++ backend/CopyZillaBackend/src/CopyZillaBackend.Application/Features/User/Commands/DeleteUserCommand/DeleteUserCommandHandler.cs
@@ -25,7 +25,7 @@ namespace CopyZillaBackend.Application.Features.User.Commands.DeleteUserCommand
         {
             var result = new DeleteUserCommandResult();
 
-            var validator = new DeleteUserCommandValidator(_userRepository, _stripeService, _firebaseService);
+            var validator = new DeleteUserCommandValidator(_userRepository);
             var validationResult = await validator.ValidateAsync(request, cancellationToken);
 
             validationResult.Resolve(result);
@@ -40,9 +40,17 @@ namespace CopyZillaBackend.Application.Features.User.Commands.DeleteUserCommand
             await _userRepository.DeleteAsync(request.UserId);
 
             // delete user from stripe
+            var customer = await _stripeService.GetCustomerByIdAsync(userFromDatabase!.StripeCustomerId);
+            if (customer == null)
+                throw new Exception($"Customer with customerId: {userFromDatabase.StripeCustomerId} does not exist in Stripe");
+
             await _stripeService.DeleteCustomerAsync(userFromDatabase.StripeCustomerId);
 
             // delete user from firebase
+            var firebaseUser = await _firebaseService.GetFirebaseUserAsync(userFromDatabase!.FirebaseUid);
+            if (firebaseUser == null)
+                throw new Exception($"Firebase user with id: {userFromDatabase.FirebaseUid} does not exist in Firebase");
+
             await _firebaseService.DeleteFirebaseUserAsync(userFromDatabase.FirebaseUid);
 
             //delete user prompt results from mongodb
--- backend/CopyZillaBackend/src/CopyZillaBackend.Application/Features/User/Commands/DeleteUserCommand/DeleteUserCommandValidator.cs
+++ backend/CopyZillaBackend/src/CopyZillaBackend.Application/Features/User/Commands/DeleteUserCommand/DeleteUserCommandValidator.cs
@@ -1,6 +1,4 @@
-﻿using CopyZillaBackend.Application.Contracts.Firebase;
-using CopyZillaBackend.Application.Contracts.Payment;
-using CopyZillaBackend.Application.Contracts.Persistence;
+﻿using CopyZillaBackend.Application.Contracts.Persistence;
 using FluentValidation;
 
 namespace CopyZillaBackend.Application.Features.User.Commands.DeleteUserCommand
@@ -8,14 +6,10 @@ namespace CopyZillaBackend.Application.Features.User.Commands.DeleteUserCommand
     public class DeleteUserCommandValidator : AbstractValidator<DeleteUserCommand>
     {
         private readonly IUserRepository _repository;
-        private readonly IStripeService _stripeService;
-        private readonly IFirebaseService _firebaseService;
 
-        public DeleteUserCommandValidator(IUserRepository repository, IStripeService stripeService, IFirebaseService firebaseService)
+        public DeleteUserCommandValidator(IUserRepository repository)
         {
             _repository = repository;
-            _stripeService = stripeService;
-            _firebaseService = firebaseService;
 
             RuleFor(e => e)
               .Must(e => e.UserId != Guid.Empty)
@@ -24,42 +18,13 @@ namespace CopyZillaBackend.Application.Features.User.Commands.DeleteUserCommand
 
             RuleFor(e => e)
                .MustAsync(ExistsInDbAsync)
-               .WithMessage("User with specified UserId does not exist in the database.")
+               .WithMessage($"User does not exist in db.")
                .WithErrorCode("404");
-
-            RuleFor(e => e)
-              .MustAsync(ExistsInStripeAsync)
-              .WithMessage("User with specified StripeCustomerId does not exist in Stripe.")
-              .WithErrorCode("404");
-
-            RuleFor(e => e)
-             .MustAsync(ExistsInFirebaseAsync)
-             .WithMessage("User with specified FirebaseUId does not exist in Firebase.")
-             .WithErrorCode("404");
         }
 
         private async Task<bool> ExistsInDbAsync(DeleteUserCommand e, CancellationToken _)
         {
             return await _repository.ExistsAsync(e.UserId);
-        }
-
-        private async Task<bool> ExistsInStripeAsync(DeleteUserCommand e, CancellationToken _)
-        {
-            // get user from db
-            var userFromDatabase = await _repository.GetByIdAsync(e.UserId);
-
-            if (userFromDatabase == null)
-                throw new Exception("");
-
-            return await _stripeService.GetCustomerByIdAsync(userFromDatabase.StripeCustomerId) is not null;
-        }
-
-        private async Task<bool> ExistsInFirebaseAsync(DeleteUserCommand e, CancellationToken _)
-        {
-            // get user from db
-            var userFromDatabase = await _repository.GetByIdAsync(e.UserId);
-
-            return await _firebaseService.GetFirebaseUserAsync(userFromDatabase!.FirebaseUid) is not null;
         }
     }
 }
