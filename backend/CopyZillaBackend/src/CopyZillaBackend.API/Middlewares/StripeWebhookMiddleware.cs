24ab865b174e0715a0ccd33b1ebfc3e24630133e
kiralyandrasdev <kiralyandrasdev@gmail.com>
2023-02-23 22:34:42 +0100

    init commit

---
+++ backend/CopyZillaBackend/src/CopyZillaBackend.API/Middlewares/StripeWebhookMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Stripe;
+
+namespace CopyZillaBackend.API.Middlewares
+{
+    public class StripeWebhookMiddleware : IMiddleware
+    {
+        private readonly IConfiguration _configuration;
+
+        public StripeWebhookMiddleware(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var signature = _configuration.GetSection("Stripe").GetValue<string>("EndpointSecret");
+
+            var json = await new StreamReader(context.Request.Body).ReadToEndAsync();
+
+            var stripeEvent = EventUtility.ConstructEvent(json,
+                context.Request.Headers["Stripe-Signature"], 
+                signature);
+
+            if (stripeEvent == null)
+            {
+                throw new StripeException("Could not verify webhook event.");
+            }
+
+            await next(context);
+        }
+    }
+}
