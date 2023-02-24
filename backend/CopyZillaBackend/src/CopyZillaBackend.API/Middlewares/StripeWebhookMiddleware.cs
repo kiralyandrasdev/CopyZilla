def89b0ee2ec5d6000e8f768ff64112c2b08a218
kiralyandrasdev <kiralyandrasdev@gmail.com>
2023-02-24 11:31:30 +0100

    stripe event validation finished

--- backend/CopyZillaBackend/src/CopyZillaBackend.API/Middlewares/StripeWebhookMiddleware.cs
+++ backend/CopyZillaBackend/src/CopyZillaBackend.API/Middlewares/StripeWebhookMiddleware.cs
@@ -15,14 +15,22 @@ namespace CopyZillaBackend.API.Middlewares
         {
             var signature = _configuration.GetSection("Stripe").GetValue<string>("EndpointSecret");
 
-            var json = await new StreamReader(context.Request.Body).ReadToEndAsync();
+            // enable buffering on the request
+            context.Request.EnableBuffering();
 
+            // leave the body open for next middlewares/controller
+            var json = await new StreamReader(context.Request.Body, leaveOpen: true).ReadToEndAsync();
+
+            // validate stripe event signature
             var stripeEvent = EventUtility.ConstructEvent(json,
-                context.Request.Headers["Stripe-Signature"], 
+                context.Request.Headers["Stripe-Signature"],
                 signature);
 
             if (stripeEvent == null)
                 throw new StripeException("Could not verify webhook event.");
+
+            // reset the request body stream position 
+            context.Request.Body.Position = 0;
 
             await next(context);
         }
