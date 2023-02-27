ab4c0b4cdc921c266333c08fc446890ed766dd25
kiralyandrasdev <kiralyandrasdev@gmail.com>
2023-02-27 22:54:10 +0100

    use test firebase project in extension

--- web_extensions/copyzilla_email_assistant/src/config/firebaseConfig.ts
+++ web_extensions/copyzilla_email_assistant/src/config/firebaseConfig.ts
@@ -1,4 +1,4 @@
-export const firebaseConfig = {
+const prodFirebaseConfig = {
     apiKey: "AIzaSyABqjrYSGB9I9yMnpDOzm2r09cPzU1Sjo4",
     authDomain: "copyzilla-f4288.firebaseapp.com",
     projectId: "copyzilla-f4288",
@@ -7,3 +7,24 @@ export const firebaseConfig = {
     appId: "1:956768885933:web:3c02f4b05595b989feba24",
     measurementId: "G-3FQX6DLE0N"
 };
+
+const devFirebaseConfig = {
+    apiKey: "AIzaSyCptuYRmGsGFKqlmxUgBI8sb95uSgA-34o",
+    authDomain: "copyzillatest.firebaseapp.com",
+    projectId: "copyzillatest",
+    storageBucket: "copyzillatest.appspot.com",
+    messagingSenderId: "499584646337",
+    appId: "1:499584646337:web:b6f5d0146efd3e56e52840"
+};
+
+export async function getFirebaseConfig() {
+    return new Promise((resolve) => {
+        chrome.management.getSelf((info) => {
+            if (info.installType === "development") {
+                resolve(devFirebaseConfig);
+            }
+
+            resolve(prodFirebaseConfig);
+        });
+    });
+}
