b8a467d98fe9dd91c8472a1553656b252d9b5841
kiralyandrasdev <kiralyandrasdev@gmail.com>
2023-02-27 22:26:38 +0100

    new credentials has been added to test firebase project

--- web/src/config/firebaseConfig.js
+++ web/src/config/firebaseConfig.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyABqjrYSGB9I9yMnpDOzm2r09cPzU1Sjo4",
-    authDomain: "copyzilla-f4288.firebaseapp.com",
-    projectId: "copyzilla-f4288",
-    storageBucket: "copyzilla-f4288.appspot.com",
-    messagingSenderId: "956768885933",
-    appId: "1:956768885933:web:3c02f4b05595b989feba24",
-    measurementId: "G-3FQX6DLE0N"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
