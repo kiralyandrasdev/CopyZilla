1402306705c65e7bc2252fe6e7b1cbce7eed2c77
kiralyandrasdev <kiralyandrasdev@gmail.com>
2023-03-19 18:43:59 +0100

    Revert "unused interface removed"
    This reverts commit 289f4795c2654ebebf568cf90ac7c1ca93febf26.

---
+++ backend/CopyZillaBackend/src/CopyZillaBackend.Application/Contracts/Persistence/ICacheService.cs
@@ -0,0 +1,10 @@
+﻿namespace CopyZillaBackend.Application.Contracts.Persistence
+{
+    public interface ICacheService
+    {
+        Task SetAsync<T>(string key, T value);
+        Task<T?> GetAsync<T>(string key);
+        Task<List<T>> GetAllValuesOfTypeAsync<T>();
+        Task RemoveAsync(string key);
+    }
+}
